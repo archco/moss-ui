011a4f6e212b2a99f84fd98e488be54763b7b156
archco <archcoster@gmail.com>
2017-07-14 11:47:56 +0900

    modify examples.

--- example/html/example.js
+++ example/html/example.js
@@ -6,3 +6,10 @@ function toastShow() {
   window.Cosmos.toast('toast test ' + toastCount);
   toastCount++;
 }
+
+// message-box.
+function showMessage() {
+  var text = document.querySelector('#input-msg').value || 'empty';
+  var status = document.querySelector('#select-msg').value;
+  window.Cosmos.messageBox.add(text, status);
+}
