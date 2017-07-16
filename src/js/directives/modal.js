6dae84fe854357a0bdec2c022f2b4a2af8e4177f
archco <archcoster@gmail.com>
2017-07-16 10:26:09 +0900

    Add 'event.preventDefault()' in modal and trigger directives.

--- src/js/directives/modal.js
+++ src/js/directives/modal.js
@@ -3,12 +3,13 @@ import { EventBus } from '../lib/event-bus';
 export default {
   name: 'modal',
   bind(el, binding) {
-    el.addEventListener('click', () => {
+    el.addEventListener('click', event => {
+      event.preventDefault();
       let action = binding.modifiers.show ? 'show'
         : binding.modifiers.close ? 'close'
         : 'toggle';
 
       EventBus.$emit('modal-toggle', binding.arg, action);
     });
-  }
+  },
 };
--- src/js/directives/trigger.js
+++ src/js/directives/trigger.js
@@ -3,8 +3,9 @@ import { EventBus } from '../lib/event-bus';
 export default {
   name: 'trigger',
   bind(el, binding) {
-    el.addEventListener('click', () => {
+    el.addEventListener('click', event => {
+      event.preventDefault();
       EventBus.$emit(binding.arg, ...binding.value);
     });
-  }
+  },
 };
