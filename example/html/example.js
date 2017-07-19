76505c57b455bb3dda14018c1db4e8aa330aea3f
archco <archcoster@gmail.com>
2017-07-19 11:40:25 +0900

    fix example/js-libraries

--- example/html/example.js
+++ example/html/example.js
@@ -1,15 +1,88 @@
 /* eslint-disable no-unused-vars */
 
-// toast.
+/************************************************************
+  Toast
+*************************************************************/
+
 var toastCount = 0;
 function toastShow() {
   window.Cosmos.toast('toast test ' + toastCount);
   toastCount++;
 }
 
-// message-box.
+/************************************************************
+  MessageBox
+*************************************************************/
+
 function showMessage() {
   var text = document.querySelector('#input-msg').value || 'empty';
   var status = document.querySelector('#select-msg').value;
   window.Cosmos.messageBox.add(text, status);
 }
+
+/************************************************************
+  Color Library
+*************************************************************/
+
+function convertColor(hexToRgb) {
+  var inputHex = document.querySelector('#input-hex-color');
+  var inputRgb = document.querySelector('#input-rgb-color');
+
+  if (hexToRgb) {
+    inputRgb.value = window.Cosmos.lib.Color.hexToRgb(inputHex.value).join(',');
+  } else {
+    var array = inputRgb.value.split(',').map(function (x) {
+      return parseInt(x);
+    });
+
+    inputHex.value = window.Cosmos.lib.Color.rgbToHex(array[0], array[1], array[2], true);
+  }
+}
+
+function getLightness() {
+  var value = window.Cosmos.lib.Color
+    .lightness(document.querySelector('#input-lightness').value);
+  document.querySelector('#output-lightness').innerHTML = 'lightness value: ' + value;
+}
+
+function getContrast() {
+  var inputValue = document.querySelector('#input-contrast').value;
+  var contrast = window.Cosmos.lib.Color.contrast(inputValue);
+  var output = document.querySelector('#output-contrast');
+
+  output.style.backgroundColor = inputValue;
+  output.style.color = contrast;
+  output.innerHTML = 'bg-color: ' + inputValue + ' contrast-color: ' + contrast;
+}
+
+/************************************************************
+  ElementUtil Library
+*************************************************************/
+
+(function () {
+  var eu = window.Cosmos.lib.ElementUtil;
+
+  // toggle class
+  eu.addListener('#btn-toggle-class', 'click', function () {
+    eu.toggleClass('#toggle-class-target', 'bg-color-danger');
+  });
+
+  // toggle show
+  eu.addListener('#btn-toggle-show', 'click', function () {
+    eu.toggleShow('#toggle-show-target');
+  });
+
+  // filter list
+  eu.addListener('#input-filter-list', 'keyup', function (e) {
+    eu.filter('#filter-list li', e.currentTarget.value);
+  });
+
+  eu.addListener('#btn-sort-list', 'click', function () {
+    eu.sortElements('#sort-list', 'li');
+  });
+
+  // sort table
+  if (eu.getElement('#sort-table-target')) {
+    eu.sortTable('#sort-table-target');
+  }
+})();
