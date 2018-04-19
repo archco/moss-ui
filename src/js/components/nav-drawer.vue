0d32abb9728d81ec2588c3a1b77daa5fd9b9c700
archco <archcoster@gmail.com>
2018-04-19 12:50:38 +0900

    Implements base part of the nav-drawer. #24

---
+++ src/js/components/nav-drawer.vue
@@ -0,0 +1,11 @@
+<template>
+  <collapse class="nav-drawer" :id="id" direction="horizontal">
+    <slot></slot>
+  </collapse>
+</template>
+
+<script>
+export default {
+  props: ['id'],
+}
+</script>
