c3c975274f78b61bc27f34aece39843c580926c9
archco <archcoster@gmail.com>
2018-04-21 14:09:48 +0900

    Implement nav-drawer. #24
    - close when clicked the outside.
    - responsive width.
    - overflow-y.

--- src/js/components/nav-drawer.vue
+++ src/js/components/nav-drawer.vue
@@ -1,11 +1,31 @@
 <template>
-  <collapse class="nav-drawer" :id="id" direction="horizontal">
+  <collapse class="nav-drawer"
+    :id="id"
+    direction="horizontal"
+    ref="collapse">
     <slot></slot>
   </collapse>
 </template>
 
 <script>
+import { addOuterListener } from 'element-util';
+
 export default {
   props: ['id'],
+  mounted() {
+    addOuterListener(
+      document.documentElement,
+      this.$el,
+      'click',
+      this.onOtherClick.bind(this),
+      true,
+    );
+  },
+  methods: {
+    onOtherClick() {
+      const c = this.$refs.collapse;
+      if (c.show) c.toggleCollapse(this.id, 'close');
+    }
+  }
 }
 </script>
